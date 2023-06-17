1757a61b4fe8dcc8613878e97174af23d75666dc
Ayaan Sharif <60233803+ayxxn-shxrif@users.noreply.github.com>
2023-06-17 22:14:32 +0000

    added the kaggle icon in svg

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -25,16 +25,24 @@ const Footer = () => {
           <li><a href="/home" target="_blank" className="contact-icon">
             <i class="fa fa-linkedin" aria-hidden="true"></i></a>
           </li>
-          <li><a href="#" target="_blank" class="contact-icon">
+          <li><a href="#" target="_blank" className="contact-icon">
             <i class="fa fa-github" aria-hidden="true"></i></a>
           </li>
-          <li><a href="#" target="_blank" class="contact-icon">
+          <li><a href="#" target="_blank" className="contact-icon">
             <i class="fa fa-twitter" aria-hidden="true"></i></a>
           </li>
-          <li><a href="#" target="_blank" class="contact-icon">
+          <li><a href="#" target="_blank" className="contact-icon">
             <i class="fa fa-telegram" aria-hidden="true"></i></a>
           </li>
-      
+          <li>
+  <a href="#" target="_blank" className="contact-icon kaggle-icon">
+    <svg xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512">
+      <path d="M304.2 501.5L158.4 320.3 298.2 185c2.6-2.7 1.7-10.5-5.3-10.5h-69.2c-3.5 0-7 1.8-10.5 5.3L80.9 313.5V7.5q0-7.5-7.5-7.5H21.5Q14 0 14 7.5v497q0 7.5 7.5 7.5h51.9q7.5 0 7.5-7.5v-109l30.8-29.3 110.5 140.6c3 3.5 6.5 5.3 10.5 5.3h66.9q5.25 0 6-3z"/>
+    </svg>
+  </a>
+</li>
+
+
           </ul>
 
 
